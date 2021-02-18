5e081a4e474277f079a52c393aa4b2e43656721a
richardso21 <richiso899@yahoo.com>
2021-02-18 14:41:02 -0500

    semi-working login page

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,45 +3,51 @@ export default {
   head: {
     title: 'full-stack-2021-asian-squad',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'vuesax/dist/vuesax.css'
-  ],
+  css: ['vuesax/dist/vuesax.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/vuesax'
-  ],
+  plugins: ['@/plugins/vuesax'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+  modules: ['@nuxtjs/axios', '@nuxtjs/firebase'],
+
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyBQ8qyDb_NCgq6dPaolSdtBlqTuFiUW5h4',
+      databaseURL: 'https://bone-apple-teeth-default-rtdb.firebaseio.com/',
+      authDomain: 'bone-apple-teeth.firebaseapp.com',
+      projectId: 'bone-apple-teeth',
+      storageBucket: 'bone-apple-teeth.appspot.com',
+      messagingSenderId: '738508615418',
+      appId: '1:738508615418:web:5c78943bda4006914e6343',
+      measurementId: 'G-KH82K5LGVK',
+    },
+    services: {
+      auth: true,
+      firestore: true,
+    },
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
---
+++ pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <form @submit.prevent>
+      <input type="text" name="email" placeholder="Email" v-model="email" />
+      <input type="text" name="pass" placeholder="Password" v-model="pass" />
+      <input type="submit" value="Log In" @click="checkLogin()" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      pass: '',
+    }
+  },
+  methods: {
+    checkLogin: function () {
+      this.$fire.auth
+        .signInWithEmailAndPassword(this.email, this.pass)
+        .then((userCredential) => {
+          alert(`Signed in as ${userCredential.user.uid}!`)
+        })
+        .catch((error) => {
+          alert(error.message)
+        })
+    },
+  },
+}
+</script>
+
+<style>
+div {
+  font-size: 5rem;
+}
+</style>
