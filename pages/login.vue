316a42f6d42edaf897ae735b9c8c78f6c70cb631
Junshen Gao <57498746+Op1a4tzd@users.noreply.github.com>
2021-02-20 16:30:20 -0500

    basic css

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,16 @@
 <template>
+<div class="background"> 
   <div>
-    <h1>Login Page</h1>
+    <h1>Bone Apple Teeth</h1>
+    <h2>Sign In</h2>
     <form @submit.prevent>
-      <input type="text" name="email" placeholder="Email" v-model="email" />
-      <input type="text" name="pass" placeholder="Password" v-model="pass" />
-      <input type="submit" value="Log In" @click="checkLogin()" />
+      <input class="flex-items" type="text" name="email" placeholder="Email" v-model="email" />
+      <input class="flex-items" type="text" name="pass" placeholder="Password" v-model="pass" />
+      <input class="flex-items" type="submit" value="Sign In" @click="checkLogin()" />
+      <input class="flex-items" type="submit" value="Sign in with Google" @click="checkLogin()" />
     </form>
   </div>
+</div>
 </template>
 
 <script>
@@ -34,7 +38,50 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Nova+Round&display=swap');
+
+.background{
+    z-index: -1;
+    content: '';
+    background-size:cover;
+    background-color: #F5A6A6;
+    /*clip-path: polygon(0 70%, 100% 30%, 100% 100%, 0% 100%);*/
+
+    
+}
+
 div {
-  font-size: 5rem;
+  font-family: 'Nova Round', cursive;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 1;
+}
+
+
+h1 {
+margin-top:5rem;
+font-size:2.5rem;
+}
+
+h2{
+font-size: 2rem;
+margin-right:10rem;
+margin-top: 5rem;
+
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: space-around;
+  margin-top: 2rem;
+}
+
+.flex-items{
+margin: 2rem;  
 }
 </style>
