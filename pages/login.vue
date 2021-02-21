e3d91d3b3befec6066d6805202449b3ee2163e03
richardso21 <richiso899@yahoo.com>
2021-02-21 17:59:38 -0500

    fix clip-pathing and vs input/buttons

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,38 @@
 <template>
-<div class="background"> 
-  <div>
+  <div class="login-content">
     <h1>Bone Apple Teeth</h1>
     <h2>Sign In</h2>
     <form @submit.prevent>
-      <input class="flex-items" type="text" name="email" placeholder="Email" v-model="email" />
-      <input class="flex-items" type="text" name="pass" placeholder="Password" v-model="pass" />
-      <input class="flex-items" type="submit" value="Sign In" @click="checkLogin()" />
-      <input class="flex-items" type="submit" value="Sign in with Google" @click="checkLogin()" />
+      <vs-input
+        class="flex-items"
+        type="text"
+        name="email"
+        label-placeholder="Email"
+        v-model="email"
+      />
+      <vs-input
+        class="flex-items"
+        type="password"
+        name="pass"
+        label-placeholder="Password"
+        v-model="pass"
+      />
+      <vs-input
+        class="flex-items"
+        type="submit"
+        value="Sign In"
+        @click="checkLogin()"
+      />
+      <vs-input
+        class="flex-items"
+        type="submit"
+        value="Sign in with Google"
+        @click="checkLogin()"
+      />
     </form>
+    <div class="background">
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -41,16 +63,17 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Nova+Round&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
 
-.background{
-  position: relative;
-    z-index: -1;
-    background-size:cover;
-    background-color: #F5A6A6;
-   /*clip-path: polygon(0 70%, 100% 30%, 100% 100%, 0% 100%) ;*/
-    
+.background {
+  position: absolute;
+  z-index: -1;
+  background-size: cover;
+  background-color: #f5a6a6;
+  clip-path: polygon(0 70%, 100% 30%, 100% 100%, 0% 100%);
+  height: 100vh;
+  width: 100vw;
 }
 
-div {
+.login-content {
   position: relative;
   font-family: 'Nova Round', cursive;
   height: 100vh;
@@ -58,40 +81,36 @@ div {
   display: flex;
   flex-direction: column;
   align-items: center;
-  z-index: 1;
-  
+  z-index: 2;
 }
-
 
 h1 {
-margin-top:5rem;
-font-size:2.5rem;
-display:block;
-z-index: 3;
-
+  margin-top: 5rem;
+  font-size: 2.5rem;
+  display: block;
+  z-index: 3;
 }
 
-h2{
-font-size: 2rem;
-margin-right:10rem;
-margin-top: 5rem;
+h2 {
+  font-size: 2rem;
+  margin-right: 10rem;
+  margin-top: 5rem;
 
-z-index: 3;
+  z-index: 3;
 }
 
 form {
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
   align-content: space-around;
   margin-top: 2rem;
 }
 
-.flex-items{
-margin: 2rem;  
-width:15rem;
+.flex-items {
+  margin: 2rem;
+  transform: scale(1.25);
+  /* width: 15rem; */
 }
-
-
-
 </style>
