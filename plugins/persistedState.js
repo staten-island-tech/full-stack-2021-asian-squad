bbdacc7529e753fcabdaa8b93a09bf2ffc95bd21
Richard So <richiso899@yahoo.com>
2021-03-08 11:07:38 -0500

    cookies bug fix

--- plugins/persistedState.js
+++ plugins/persistedState.js
@@ -1,6 +1,6 @@
-import createPersistedState from 'vuex-persistedstate';
-import * as Cookies from 'js-cookie';
-import cookie from 'cookie';
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
+import cookie from 'cookie'
 
 // persists state in between reloads using cookies
 // works with Nuxt SSR :D
@@ -8,16 +8,22 @@ export default ({ store, req, isDev }) => {
   createPersistedState({
     storage: {
       getItem: (key) => {
+        // use cookies in request headers if in server
         if (process.server) {
-          const parsedCookies = cookie.parse(req.headers.cookie);
-          return parsedCookies[key];
+          // return nothing if cookies not present
+          const reqCookies = req.headers.cookie
+          if (!reqCookies) return
+          // parse + return cookies if present
+          const parsedCookies = cookie.parse(reqCookies)
+          return parsedCookies[key]
         } else {
-          return Cookies.get(key);
+          // get cookies if in client
+          return Cookies.get(key)
         }
       },
       setItem: (key, value) =>
         Cookies.set(key, value, { expires: 365, secure: !isDev }),
-      removeItem: key => Cookies.remove(key)
-    }
-  })(store);
-};
+      removeItem: (key) => Cookies.remove(key),
+    },
+  })(store)
+}
