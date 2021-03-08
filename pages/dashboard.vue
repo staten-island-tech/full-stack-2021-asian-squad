b9b90aab49c810c4b0e3505601d178f7f1626ea2
Iram Liu <64221371+ili23@users.noreply.github.com>
2021-03-08 11:06:31 -0500

    asdfsda

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,20 +1,39 @@
 <template>
-  <div id="dashboard"> 
-      <h3>Dashboard</h3>
+  <div id="dashboard">
+    <h3>{{users.id}}</h3>
   </div>
 </template>
 
 <script>
+
 export default {
-    name: 'dashboard',
-    data () {
-        return{
-            
-        }
+  name: 'dashboard',
+  data () {
+    return {
+      users: []
     }
+  },
+  created () {
+    this.$fire.firestore
+      .collection('user')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const data = {
+            'id': doc.id,
+            'uname': doc.data().uname,
+            'udescription': doc.data().udescription,
+          }
+          this.users.push(data)
+        })
+      })
+  },
 }
 </script>
 
 <style>
-
+.user-item{
+  border: 1rem;
+  border-color: black;
+}
 </style>
